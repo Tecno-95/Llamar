87c1a1d9f9bbdba3613ddfa533837c5b22b07219
Tecno-95 <74727783+Tecno-95@users.noreply.github.com>
2022-05-02 01:50:13 -0500

    Update Llamadas.py

--- Llamadas.py
+++ Llamadas.py
@@ -4,6 +4,6 @@ import subprocess
 import time
 
 numero = input("[+]Numero de télefono: ")
-subprocess.call("termux-telephony-call {}".format(numero),shell=True
+subprocess.call("termux-telephony-call {}".format(numero),shell=True)
 time.sleep(3)
 print("\u001b[32;1m[+]Llamando...")
